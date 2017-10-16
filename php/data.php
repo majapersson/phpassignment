da81dc9c9ec45eff210789d29254502e3019ec68
Maja <persson.maja@gmail.com>
2017-10-16 11:43:30 +0200

    Change keys to lowercase

--- php/data.php
+++ php/data.php
@@ -6,16 +6,17 @@ declare(strict_types=1);
 
 $posts = [
   [
-    'Title' => "There's pros and cons to every alternate timeline.",      // string
-    'Content' => "Well I don't like your unemployed genes in my grandchildren, Jerry. If you break the rules, try to leave or lose the game, you will die. Just like Saaaaw. I mixed in some praying mantis DNA. You know a praying mantis is the exact opposite of a vole, Morty? They mate once and then bluergh cut each other's heads off. Because I don't respect therapy; because I'm a scientist; because I invent, transform, create, and destroy for a living, and when I don't like something about the world, I change it. And I don't think going to a rented office in a strip mall to listen to some agent of averageness explain which words mean which feelings has ever helped anyone do anything. I think it's helped a lot of people get comfortable and stop panicking, which is a state of mind we value in the animals we eat, but not something I want for myself. I'm not a cow, I'm a pickle... when I feel like it.
-
+    'title' => "There's pros and cons to every alternate timeline.",      // string
+    'content' => "Well I don't like your unemployed genes in my grandchildren, Jerry. If you break the rules, try to leave or lose the game, you will die. Just like Saaaaw. I mixed in some praying mantis DNA. You know a praying mantis is the exact opposite of a vole, Morty? They mate once and then bluergh cut each other's heads off. Because I don't respect therapy; because I'm a scientist; because I invent, transform, create, and destroy for a living, and when I don't like something about the world, I change it. And I don't think going to a rented office in a strip mall to listen to some agent of averageness explain which words mean which feelings has ever helped anyone do anything. I think it's helped a lot of people get comfortable and stop panicking, which is a state of mind we value in the animals we eat, but not something I want for myself. I'm not a cow, I'm a pickle... when I feel like it.
     Rubber baby buggy bumpers! Hi! I'm Mr Meeseeks! Look at me! You don't have to try to impress me Morty. Those guys are inside you building a piece of shit Ethan! They're inside you building a monument to compromise! Fuck them, fuck those people, fuck this whole thing Ethan.",    //string
-    'Author' => [       // array
-      'ID' => 1,        // int
-      'Full Name' => 'Rick Sanchez' // string
+
+    'author' => [       // array
+      'id' => 1,        // int
+      'fullName' => 'Rick Sanchez' // string
     ],
-    'Date' => '2017-10-16',       // string
-    'Likes' => 1
+
+    'date' => '2017-10-16',       // string
+    'likes' => 1
   ]
 
 ];
