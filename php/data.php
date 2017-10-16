491196067be7c28b187c758dd742800525587fe8
Maja <persson.maja@gmail.com>
2017-10-16 11:14:52 +0200

    Add

--- php/data.php
+++ php/data.php
@@ -5,5 +5,15 @@ declare(strict_types=1);
 // This file is for arrays, such as posts, authors and info
 
 $posts = [
+  [
+    'Title' => '',      // string
+    'Content' => '',    // string
+    'Author' => [       // array
+      'ID' => 1,        // int
+      'Full Name' => '' // string
+    ],
+    'Date' => '',       // string
+    'Likes' => 1    
+  ]
 
 ];
