7e915d525581870329772a766ae42c5d5529c89e
Maja <persson.maja@gmail.com>
2017-10-16 12:08:32 +0200

    Added style to main and article

--- index.php
+++ index.php
@@ -7,31 +7,32 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="/css/master.css">
-  <title>Document</title>
+  <title>The best Rick and Morty Blog ever!</title>
 </head>
 <body>
+  <main>
+    <?php foreach ($posts as $post): ?>
 
-  <?php foreach ($posts as $post): ?>
+      <article>
+        <h2>
+          <?php echo $post['title'] ?>
+        </h2>
+        <h3>
+          <?php echo $post['author']['fullName'] ?>
+        </h3>
+        <time>
+          <?php echo $post['date'] ?>
+        </time>
+        <p>
+          <?php echo $post['content'] ?>
+        </p>
+        <p>Likes:
+          <?php echo $post['likes'] ?>
+        </p>
 
-    <div>
-      <h1>
-        <?php echo $post['title'] ?>
-      </h1>
-      <h2>
-        <?php echo $post['author']['fullName'] ?>
-      </h2>
-      <time>
-        <?php echo $post['date'] ?>
-      </time>
-      <p>
-        <?php echo $post['content'] ?>
-      </p>
-      <p>Likes:
-        <?php echo $post['likes'] ?>
-      </p>
+      </article>
 
-    </div>
-
-  <?php endforeach; ?>
+    <?php endforeach; ?>
+  </main>
 </body>
 </html>
