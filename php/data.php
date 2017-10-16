0e6c29e0b8b5b39c67e028ea429c98c46776dd93
Maja <persson.maja@gmail.com>
2017-10-16 10:39:04 +0200

    Add php folder with index.php, data.php, functions.php

---
+++ php/data.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// This file is for arrays, such as posts, authors and info
+
+$posts = [
+
+];
---
+++ php/functions.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// This file is for all functions
+
+function getUser() {
+
+}
