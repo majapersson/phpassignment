7ad9ff6d517c34934d8c4cb46bdf807deaadff04
Maja <persson.maja@gmail.com>
2017-10-16 11:56:21 +0200

    Add second post

--- php/data.php
+++ php/data.php
@@ -3,12 +3,29 @@
 declare(strict_types=1);
 
 // This file is for arrays, such as posts, authors and info
+// Every $post contains title, content, author, date and likes
 
-$posts = [
+    // $posts = [
+    //   [
+    //     'title' => '',
+    //     'content' => '',
+    //     'author' => [
+    //       'id' => '',
+    //       'fullName' => ''
+    //     ],
+    //
+    //     'date' => '',
+    //     'likes' => '',
+    //   ],
+    // ];
+
+
+$posts =
+[
   [
     'title' => "There's pros and cons to every alternate timeline.",      // string
     'content' => "Well I don't like your unemployed genes in my grandchildren, Jerry. If you break the rules, try to leave or lose the game, you will die. Just like Saaaaw. I mixed in some praying mantis DNA. You know a praying mantis is the exact opposite of a vole, Morty? They mate once and then bluergh cut each other's heads off. Because I don't respect therapy; because I'm a scientist; because I invent, transform, create, and destroy for a living, and when I don't like something about the world, I change it. And I don't think going to a rented office in a strip mall to listen to some agent of averageness explain which words mean which feelings has ever helped anyone do anything. I think it's helped a lot of people get comfortable and stop panicking, which is a state of mind we value in the animals we eat, but not something I want for myself. I'm not a cow, I'm a pickle... when I feel like it.
-    Rubber baby buggy bumpers! Hi! I'm Mr Meeseeks! Look at me! You don't have to try to impress me Morty. Those guys are inside you building a piece of shit Ethan! They're inside you building a monument to compromise! Fuck them, fuck those people, fuck this whole thing Ethan.",    //string
+    You don't have to try to impress me Morty. Those guys are inside you building a piece of shit Ethan! They're inside you building a monument to compromise! Fuck them, fuck those people, fuck this whole thing Ethan.",    //string
 
     'author' => [       // array
       'id' => 1,        // int
@@ -16,7 +33,21 @@ $posts = [
     ],
 
     'date' => '2017-10-16',       // string
-    'likes' => 1
+    'likes' => 1,
+  ],
+
+  [
+    'title' => "Sometimes science is a lot more art, than science. ",
+    'content' => "I don't get it, and I don't need to. Oh, it gets darker, Morty. Welcome to the darkest year of our adventures. First thing that's different? No more dad, Morty. If it were, you could call ME Ernest Hemingway. Ew, Grandpa, so gross! You're talking about my mom!
+    Aw, man! I really liked this life! Well, at least I didn't really crap my pants. Save it for the Semantics Dome, E.B.White. Ooh, your little flappy doodles are twitching. Does that mean you're aroused, or did you just get a signal that one of your buddies found a grape? What are you looking at, mother fucker!",
+    'author' => [
+      'id' => 2,
+      'fullName' => 'Pickle Rick'
+    ],
+
+    'date' => '2017-10-17',
+    'likes' => 5,
   ]
+
 
 ];
