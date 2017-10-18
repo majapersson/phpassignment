8c244f6cf15fdd51cb453c6eff3b5f4122297b94
Maja <persson.maja@gmail.com>
2017-10-18 13:24:51 +0200

    Add To Do-list

--- php/functions.php
+++ php/functions.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 // This file is for all functions
 
+// TO DO:
+// - Open file
+// - Read from file
+// - Write to file
+// - Add Like-function
+
 function getAuthor(array $post): array {
   return $post['author'];
 }
