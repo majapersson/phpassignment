ddae3873e48979692270bd8836e6c42405d3562f
Maja Persson <persson.maja@gmail.com>
2017-10-27 10:55:35 +0200

    Fix path

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="/css/master.css">
+  <link rel="stylesheet" href="css/master.css">
   <title>The best blog ever!</title>
 </head>
 <body>
